13942e8d44e3eaad8d18fae0b821dca412098c63
Bunn Nget <mr_nget@hotmail.com>
2023-07-19 14:13:15 +1000

    Addressed tfsec errors and warnings

--- main.tf
+++ main.tf
@@ -1,4 +1,28 @@
-resource "aws_s3_bucket" "b" {
+#tfsec:ignore:aws-s3-enable-versioning
+#tfsec:ignore:aws-s3-encryption-customer-key 
+
+resource "aws_s3_bucket" "cloud-resume-site-bucket" {
   bucket = "tf-aws-mr-nget-1-cloud-resume"
-  acl    = "public"
+  logging {
+        target_bucket = "<target-bucket>"
+    }
+
+  server_side_encryption_configuration {
+    rule {
+      #encrypt/decrypts whole bucket at one time; saves cost.
+      bucket_key_enabled = true
+
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "cloud-resume-site-bucket" {
+  bucket = aws_s3_bucket.cloud-resume-site-bucket.id
+  block_public_acls = true
+  block_public_policy = true 
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }

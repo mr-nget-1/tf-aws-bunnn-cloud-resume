77e70a8090a7fcffcf4376b5f0f889742e67ef3a
Bunn Nget <mr_nget@hotmail.com>
2023-07-20 11:38:41 +1000

    Added logging and tf fmt

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-#tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-encryption-customer-key
+#tfsec:ignore:aws-s3-enable-versioning
+#tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket" "cloud-resume-site-bucket" {
   bucket = "tf-aws-mr-nget-1-cloud-resume"
-  #logging {
-  #      target_bucket = "<target-bucket>"
-  #  }
+  logging {
+    target_bucket = aws_s3_bucket.cloud-resume-logging-bucket.id
+  }
 
   server_side_encryption_configuration {
     rule {
@@ -11,16 +12,45 @@ resource "aws_s3_bucket" "cloud-resume-site-bucket" {
       bucket_key_enabled = true
 
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
+        sse_algorithm = "AES256"
       }
     }
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "cloud-resume-site-bucket" {
-  bucket = aws_s3_bucket.cloud-resume-site-bucket.id
-  block_public_acls = true
-  block_public_policy = true 
-  ignore_public_acls = true
+  bucket                  = aws_s3_bucket.cloud-resume-site-bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+#tfsec:ignore:aws-s3-enable-bucket-logging
+#tfsec:ignore:aws-s3-encryption-customer-key
+resource "aws_s3_bucket" "cloud-resume-logging-bucket" {
+  bucket = "tf-aws-mr-nget-1-cloud-resume"
+
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      #encrypt/decrypts whole bucket at one time; saves cost.
+      bucket_key_enabled = true
+
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "cloud-resume-logging-bucket" {
+  bucket                  = aws_s3_bucket.cloud-resume-logging-bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }

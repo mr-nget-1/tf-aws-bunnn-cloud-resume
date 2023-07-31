fa78c5c59db3c26ec5611382be3c83cb722153b8
Bunn Nget <mr_nget@hotmail.com>
2023-07-31 16:40:09 +1000

    copied end of Ep3 code

--- nonprod/backend.tf
+++ nonprod/backend.tf
@@ -25,6 +25,6 @@ terraform {
     key     = "terraform.tfstate"
     region  = "ap-southeast-2"
     encrypt = true
-    dynamodb_table = "terraform-state-lock"
+    dynamodb_table = "terraform-state-lock-dynamo"
   }
 }

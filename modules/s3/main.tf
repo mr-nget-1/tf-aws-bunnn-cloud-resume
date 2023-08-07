bf4a5960300dc538776c76a98e658c5e4cfe28fa
Bunn Nget <mr_nget@hotmail.com>
2023-08-07 18:09:45 +1000

    Introduced CloudFront; + aws_cloudfront_origin_access_identity; aws_cloudfront_distribution; update bucket policy

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -53,6 +53,77 @@ resource "aws_s3_bucket_website_configuration" "cloud_resume_site_bucket" {
   }
 }
 
+
+
+
+#CloudFront Distribution
+resource "aws_cloudfront_origin_access_identity" "cloud_resume_site_bucket" {
+  comment = "Used for the cloud_resume_site_bucket"
+}
+
+resource "aws_cloudfront_distribution" "cloud_resume_site_bucket" {
+  origin {
+    domain_name = aws_s3_bucket.cloud_resume_site_bucket.bucket_regional_domain_name
+    origin_id   = "myS3CloudResumeSiteOrigin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.cloud_resume_site_bucket.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Cloud Resume Site Bucket"
+  default_root_object = "index.html"
+
+    logging_config {
+    include_cookies = false
+    bucket          = aws_s3_bucket.cloud_resume_logging_bucket.domain_name
+    prefix          = "cloud-resume-cf-logs"
+  }
+
+  # AWS Managed Caching Policy (CachingDisabled)
+  default_cache_behavior {
+    # Using the CachingDisabled managed policy ID:
+    cache_policy_id  = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "myS3CloudResumeSiteOrigin"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+}
+
+
+#AWS S3 Bucket Policy - cloud_resume_site_bucket
+#Origin Access Identity - Updating Bucket Policy
+resource "aws_s3_bucket_policy" "cloud_resume_site_bucket" {
+  bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+  policy = data.aws_iam_policy_document.cloud_resume_site_bucket.json
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.cloud_resume_site_bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.cloud_resume_site_bucket.iam_arn]
+    }
+  }
+}
+
+
 #tfsec:ignore:aws-s3-enable-bucket-logging
 #tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket" "cloud_resume_logging_bucket" {
